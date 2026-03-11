58b128c565d6647a50a2020c96364950b51ee6bf
Sofiiakus2 <sofiiakussss@gmail.com>
2026-03-11 14:17:13 +0200

    added animation into navigation panel

--- lib/features/navigation_panel/presentation/widgets/pages_list.dart
+++ lib/features/navigation_panel/presentation/widgets/pages_list.dart
@@ -1,5 +1,6 @@
 import 'package:cv_portfolio/core/routes/skroll_keys.dart';
 import 'package:flutter/material.dart';
+import 'package:visibility_detector/visibility_detector.dart';
 
 class PagesList extends StatefulWidget {
   const PagesList({super.key});
@@ -8,7 +9,11 @@ class PagesList extends StatefulWidget {
   State<PagesList> createState() => _PagesListState();
 }
 
-class _PagesListState extends State<PagesList> {
+class _PagesListState extends State<PagesList>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+  bool _animated = false;
+
   late final List<(String, GlobalKey)> _pages = [
     ('who am I?', ScrollKeys.whoAmI),
     ('education', ScrollKeys.education),
@@ -17,6 +22,43 @@ class _PagesListState extends State<PagesList> {
     ('portfolio', ScrollKeys.portfolio),
     ('contacts', ScrollKeys.contacts),
   ];
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 2500),
+      vsync: this,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _onVisibilityChanged(VisibilityInfo info) {
+    if (info.visibleFraction > 0.5 && !_animated) {
+      _animated = true;
+      _controller.forward();
+    }
+  }
+
+  Animation<Offset> _slideAnimation(int index) {
+    final start = index / _pages.length;
+    final end = (index + 1) / _pages.length;
+
+    return Tween<Offset>(
+      begin: const Offset(-20, 0),
+      end: Offset.zero,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Interval(start, end, curve: Curves.easeOut),
+      ),
+    );
+  }
 
   void _scrollTo(GlobalKey key) {
     final context = key.currentContext;
@@ -30,21 +72,30 @@ class _PagesListState extends State<PagesList> {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: _pages
-          .map((page) => GestureDetector(
-        onTap: () => _scrollTo(page.$2),
-        child: Text(
-          page.$1,
-          style: Theme.of(context).textTheme.labelLarge?.copyWith(
-            decoration: TextDecoration.underline,
-            decorationColor:
-            Theme.of(context).textTheme.labelLarge?.color,
+    return VisibilityDetector(
+      key: const Key('pages_list'),
+      onVisibilityChanged: _onVisibilityChanged,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: _pages.indexed
+            .map((item) => SlideTransition(
+          position: _slideAnimation(item.$1),
+          child: GestureDetector(
+            onTap: () => _scrollTo(item.$2.$2),
+            child: Text(
+              item.$2.$1,
+              style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                decoration: TextDecoration.underline,
+                decorationColor: Theme.of(context)
+                    .textTheme
+                    .labelLarge
+                    ?.color,
+              ),
+            ),
           ),
-        ),
-      ))
-          .toList(),
+        ))
+            .toList(),
+      ),
     );
   }
 }
